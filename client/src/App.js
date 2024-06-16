534cc30e375ff6b46c42bbf5ad81dd02b9249d47
Praveenan Jeevarethinam <praveenanjvp@gmail.com>
2024-06-16 09:11:55 +0530

    #4.5, Logout functionality has implemented

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,20 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import Register from './pages/Register';
+import { AuthContext } from './context/AuthContext';
+import { useContext } from 'react';
+
 
 
 function App() {
-  
+  const {user} = useContext(AuthContext)
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<LoginPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/home' element={<HomePage/>}/>
+        {/* <Route path='/home' element={<HomePage/>}/> */}
+        <Route path='/home' element={user ? <HomePage/> : <LoginPage/>}/>
         <Route path='/register' element={<Register/>}/>
       </Routes>
     </BrowserRouter>
--- client/src/components/TopNavBar.jsx
+++ client/src/components/TopNavBar.jsx
@@ -2,6 +2,13 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 export default function TopNavBar() {
+  const {user,dispatch} = useContext(AuthContext);
+  const handleSignUp = () =>{
+   
+    dispatch({
+      type : "LOGOUT"
+    })
+  }
 
   return (
     <div className="bg-dark-blue p-4 w-full">
@@ -21,7 +28,7 @@ export default function TopNavBar() {
         
         <div className=" flex justify-end ">
           <button className="bg-light-blue text-dark-blue md:px-8 px-4 rounded-lg transform hover:scale-110 duration-150 font-mont font-semibold "
-          >
+          onClick={handleSignUp}>
             Sign Up
           </button>
         </div>
